ed1bde987737471c1494c16bebefd4a1a5540c3a
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2017-05-15 12:56:32 +0200

    Script de publicación de chistes desde la cola

---
+++ scripts/publish_item.js
@@ -0,0 +1,17 @@
+var conn = new Mongo();
+var db = conn.getDB('fch');
+
+var item = db.cola.find().sort({ fecha: -1 }).limit(1).toArray()[0];
+
+if (!item) quit();
+
+item.fecha = new Date();
+item.ups = 0;
+item.downs = 0;
+item.valoracion = 0;
+if (item.texto.length <= 200) {
+  item.corto = true;
+}
+
+db.chistes.insertOne(item);
+db.cola.deleteOne({ _id: item._id });
