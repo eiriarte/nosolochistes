c4bdcecfa72b5d5ed3037f86e86c7b9b7a8c8917
Eduardo Iriarte Gahete <eiriarte@gmail.com>
2024-06-21 12:50:03 +0200

    Use dotenv

--- config/logger.js
+++ config/logger.js
@@ -1,12 +1,13 @@
-const onFinished = require('on-finished');
-const Logger = require('le_node');
-const config = require('./');
+const onFinished = require("on-finished");
+const Logger = require("le_node");
+const config = require("./");
+require("dotenv").config();
 
 const leLogger = new Logger({
-  token:'e4f3a8e9-c2a9-4877-9297-2229e50657b0',
+  token: process.env.LOGGER_TOKEN,
   secure: true,
   withStack: true,
-  debug: !config.production
+  debug: !config.production,
 });
 
 const log = config.production ? leLogger : console;
@@ -16,30 +17,32 @@ exports.logRequest = (req, res, next) => {
   const start = Date.now();
   onFinished(res, () => {
     const request = {};
-    request['remote_addr'] = getip(req);
-    request['date'] = (new Date()).toISOString();
-    request['method'] = req.method;
-    request['url'] = req.originalUrl || req.url;
-    request['http_version'] = req.httpVersionMajor + '.' + req.httpVersionMinor;
-    request['status'] = headersSent(res) ? res.statusCode : undefined;
-    request['referrer'] = req.headers['referer'] || req.headers['referrer'];
-    if (!request['referrer']) delete request['referrer'];
-    request['user_agent'] = req.headers['user-agent'];
-    request['response_time'] = Date.now() - start;
+    request["remote_addr"] = getip(req);
+    request["date"] = new Date().toISOString();
+    request["method"] = req.method;
+    request["url"] = req.originalUrl || req.url;
+    request["http_version"] = req.httpVersionMajor + "." + req.httpVersionMinor;
+    request["status"] = headersSent(res) ? res.statusCode : undefined;
+    request["referrer"] = req.headers["referer"] || req.headers["referrer"];
+    if (!request["referrer"]) delete request["referrer"];
+    request["user_agent"] = req.headers["user-agent"];
+    request["response_time"] = Date.now() - start;
     log.info(request);
   });
   next();
 };
 
-function getip (req) {
-  return req.ip ||
+function getip(req) {
+  return (
+    req.ip ||
     req._remoteAddress ||
     (req.connection && req.connection.remoteAddress) ||
-    undefined;
+    undefined
+  );
 }
 
-function headersSent (res) {
-  return typeof res.headersSent !== 'boolean'
+function headersSent(res) {
+  return typeof res.headersSent !== "boolean"
     ? Boolean(res._header)
     : res.headersSent;
 }
